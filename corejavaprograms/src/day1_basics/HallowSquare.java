fcacfe4db083d6d8ed189c0eaf1be3d085f59c60
balq52 <balqeesimdhad@gmail.com>
2025-08-14 02:19:28 -0700

    Add files via upload

---
+++ corejavaprograms/src/day1_basics/HallowSquare.java
@@ -0,0 +1,22 @@
+package day1_basics;
+
+public class HallowSquare {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");  
+                }
+            }
+            System.out.println();
+        }
+
+	}
+
+}
